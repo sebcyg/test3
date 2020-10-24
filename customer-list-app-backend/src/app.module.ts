bc9b55cf483d7302181854fbf4254c58d0a6969b
sebcyg <sebcyg@gmail.com>
2020-10-24 23:39:19 +0000

    sss

--- customer-list-app-backend/src/app.module.ts
+++ customer-list-app-backend/src/app.module.ts
@@ -6,7 +6,7 @@ import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
-      MongooseModule.forRoot('mongodb://locahost/customer-app', {useNewUrlParser: true}),
+      MongooseModule.forRoot('mongodb://localhost/customer-app', {useNewUrlParser: true}),
       CustomerModule
   ],
   controllers: [AppController],
--- customer-list-app-frontend/src/router/index.js
+++ customer-list-app-frontend/src/router/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeComponent from '@/views/Home';
 import EditComponent from '@/components/customer/Edit';
 import CreateComponent from '@/components/customer/Create';
-Vue.use(Router)
-export default new Router({
-  mode: 'history',
-  routes: [
+
+const routes = [
     { path: '/', redirect: { name: 'home' } },
     { path: '/home', name: 'home', component: HomeComponent },
     { path: '/create', name: 'Create', component: CreateComponent },
     { path: '/edit/:id', name: 'Edit', component: EditComponent },
-  ]
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
 });
+
+export default router;
